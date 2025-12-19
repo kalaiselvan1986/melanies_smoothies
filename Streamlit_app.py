045ba95a9f4088792fa8eae0ea4a2adbf7b71401
kalaiselvan1986 <kalaiselvan11@gmail.com>
2025-12-19 11:43:00 +0530

    Update Streamlit_app.py
    2 lines included in the fruit block

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -24,13 +24,14 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-if ingredients_list:
-    
+if ingredients_list:    
     ingredients_string = ''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothifroot_response.json(), use_container_width=True)
+   
+  #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -44,7 +45,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered ' + name_on_order + '!' , icon="✅")
 
+
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
+
+
 sf_df = st.dataframe(data=smoothifroot_response.json(), use_container_width=True)
